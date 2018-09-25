80cb526d94e1383165981fe3ed9dbd471cd2e5af
Patric Khiev <pat@Patrics-MacBook-Pro.local>
2018-09-25 15:00:22 -0500

    project complete

--- practice.js
+++ practice.js
@@ -19,7 +19,9 @@ var arr = [10,20,30];
 
 //Code Here
 
-
+function first(arr){
+  return arr[0];
+}
 
 ////////// PROBLEM 2 //////////
 
@@ -33,7 +35,9 @@ var arr = [40,50,60];
 */
 
 //Code Here
-
+function last(arr){
+  return arr[arr.length-1];
+}
 
 
 ////////// PROBLEM 3 //////////
@@ -49,7 +53,11 @@ var family = ['Tyler', 'Jordyn', 'Ryan', 'Chelsey', 'Ireland'];
 
 //Code Here
 
-
+function looper(family){
+  for(i = 0; i<family.length; i++){
+  alert(family[i]);
+  }
+}
 
 ////////// PROBLEM 4 //////////
 
@@ -63,7 +71,11 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
 */
 
 //Code Here
-
+function reversedLooper(letters){
+  for(let i=letters.length-1; i>-1; i--){
+    alert(letters[i]);
+  }
+}
 
 
 ////////// PROBLEM 5 //////////
@@ -78,9 +90,16 @@ var nums = [1,2,3,6,22,98,45,23,22,12];
 */
 
 //Code Here
+// we are iterating backwards, then removing the NOT EVEN nums via spice, then returning nums
 
-
-
+function evenFinder(nums){
+  for(let i=letters.length-1; i>-1; i--){
+    if(nums[i] % 2 !== 0){
+      nums.splice(i,1)
+    }
+  }
+  return nums;
+};
 
 
 
@@ -106,6 +125,10 @@ var numbersArray = [1,2,34,54,55,34,32,11,19,17,54,66,13];
 */
 
 //Code Here
+
+// function divider(numbersArray){
+//   return 
+// }
 
 
 
@@ -315,7 +338,3 @@ var user1 = {
 //Code Here
 
 
-
-/*
-  The activity we just did is very much how data works in 'the real world'.
-*/
